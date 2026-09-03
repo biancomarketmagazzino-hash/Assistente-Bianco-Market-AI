35e13e471916d5770dff6cc6796aac1dcf0e19ac
biancomarketmagazzino-hash <biancomarketmagazzino@gmail.com>
2026-09-03 16:10:13 +0200

    db_engine.py

--- db_engine.py
+++ db_engine.py
@@ -1,8 +1,40 @@
-def load_data_from_uploads(file_art, file_sit, file_stor):
+import duckdb
+import pandas as pd
+import os
+
+FILIALI_MAP = {
+    '00': 'Magazzino',
+    '01': 'Menfi',
+    '02': 'Mazara del Vallo',
+    '03': 'Marsala',
+    '04': 'Casa Market',
+    '05': 'Sabella',
+    '06': 'Sciacca',
+    '07': 'Ragusa',
+    '08': 'Sport Market',
+    '09': 'Trapani'
+}
+
+def find_file(filename):
+    base_dir = os.path.dirname(os.path.abspath(__file__))
+    possible_paths = [
+        os.path.join(base_dir, "data", filename),
+        os.path.join(base_dir, filename),
+        os.path.join(base_dir, "data", filename.lower()),
+        os.path.join(base_dir, filename.lower()),
+        os.path.join(base_dir, "data", filename.upper()),
+        os.path.join(base_dir, filename.upper()),
+    ]
+    for path in possible_paths:
+        if os.path.exists(path):
+            return path
+    raise FileNotFoundError(f"File {filename} non trovato.")
+
+def parse_data(art_source, sit_source, stor_source):
     con = duckdb.connect(database=':memory:')
 
     # 1. ARTICOLI
-    df_art = pd.read_csv(file_art, sep='\t', header=None, dtype=str, on_bad_lines='skip')
+    df_art = pd.read_csv(art_source, sep='\t', header=None, dtype=str, on_bad_lines='skip')
     df_art = df_art.iloc[:, [0, 1, 2, 3, 4, 5, 6, 8, 9, 21]]
     df_art.columns = ['codice', 'barcode', 'descrizione', 'um', 'categoria', 'marca', 'fornitore', 'iva', 'prezzo_base', 'costo_base']
     df_art['prezzo_base'] = pd.to_numeric(df_art['prezzo_base'], errors='coerce').fillna(0)
@@ -10,7 +42,7 @@ def load_data_from_uploads(file_art, file_sit, file_stor):
     con.register('articoli', df_art)
 
     # 2. SIT_FILIALI
-    df_sit = pd.read_csv(file_sit, sep='\t', header=None, dtype=str, on_bad_lines='skip')
+    df_sit = pd.read_csv(sit_source, sep='\t', header=None, dtype=str, on_bad_lines='skip')
     cols_idx = [0, 1, 3, 5, 7, 9, 11, 13, 15, 17, 19]
     df_sit = df_sit.iloc[:, cols_idx]
     df_sit.columns = ['codice', 'Magazzino', 'Sciacca', 'Menfi', 'Marsala', 'Trapani', 'Ragusa', 'Sabella', 'Mazara del Vallo', 'Casa Market', 'Sport Market']
@@ -19,7 +51,7 @@ def load_data_from_uploads(file_art, file_sit, file_stor):
     con.register('giacenze', df_sit)
 
     # 3. STOR_CAR
-    df_stor = pd.read_csv(file_stor, sep='\t', header=None, dtype=str, on_bad_lines='skip')
+    df_stor = pd.read_csv(stor_source, sep='\t', header=None, dtype=str, on_bad_lines='skip')
     df_stor = df_stor.iloc[:, [0, 1, 2, 4, 5, 6, 7, 8, 23]]
     df_stor.columns = ['tipo_mov', 'data', 'doc_rif', 'codice', 'qta', 'listino', 'sconto', 'prezzo_effettivo', 'filiale_cod']
     df_stor = df_stor[df_stor['tipo_mov'].isin(['S', 'F', 'T'])].copy()
@@ -30,3 +62,12 @@ def load_data_from_uploads(file_art, file_sit, file_stor):
     con.register('storico_movimenti', df_stor)
 
     return con
+
+def load_data():
+    path_articoli = find_file("ARTICOLI.TXT")
+    path_sit = find_file("Sit_filiali.TXT")
+    path_stor = find_file("STOR_CAR.txt")
+    return parse_data(path_articoli, path_sit, path_stor)
+
+def load_data_from_uploads(file_art, file_sit, file_stor):
+    return parse_data(file_art, file_sit, file_stor)
